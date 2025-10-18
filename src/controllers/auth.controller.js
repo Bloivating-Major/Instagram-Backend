6ed7012f99d45ef7448daab83070e6bec97599a8
Sambhav <sambhav12wakhariya@gmail.com>
2025-10-18 10:30:45 +0530

    feat(auth): add register controller for user signup

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,25 @@
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken")
+
+async function registerController(req, res){
+    const {username, password} = req.body;
+
+    const existingUser = await userModel.findOne({username});
+
+    if(existingUser) return res.status(409).json({message : "Username already exist"});
+
+    const user= await userModel.create({
+        username,
+        password
+    })
+
+    const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET);
+
+    res.cookie('token', token);
+
+    res.status(201).json({message : "User Created Successfully", user});
+}
+
+module.exports = {
+    registerController
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const userModel = require('../models/user.model.js');
-const jwt = require("jsonwebtoken")
-
 const authRouter = express.Router();
+const {registerController} = require("../controllers/auth.controller");
 
 /**
  * POST /register
@@ -10,23 +8,6 @@ const authRouter = express.Router();
  * GET /user [protected]
  */
 
-authRouter.post("/register", async (req, res) =>{
-    const {username, password} = req.body;
-
-    const existingUser = await userModel.findOne({username});
-
-    if(existingUser) return res.status(409).json({message : "Username already exist"});
-
-    const user= await userModel.create({
-        username,
-        password
-    })
-
-    const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET);
-
-    res.cookie('token', token);
-
-    res.status(201).json({message : "User Created Successfully", user});
-})
+authRouter.post("/register", registerController);
 
 module.exports = authRouter;
